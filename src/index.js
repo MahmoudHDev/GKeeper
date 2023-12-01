d7c9d67e1bb7a5d94d5d934dc2cb813e7744a009
Mahmoud Hashim <mahmoud.ios.dev@gmail.com>
2023-12-01 14:33:19 +0200

    adding the main files and the app file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/App.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Hello World</h1>);
+root.render(
+    <App />
+);
